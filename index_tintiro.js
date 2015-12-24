167c63e8d0696be3f17293846a9e2604fa280105
hiba12 <hiba1012hiba@gmail.com>
2015-12-24 22:12:39 +0900

    修正

--- index_tintiro.js
+++ index_tintiro.js
@@ -32,45 +32,47 @@ function afterLoad() {
         case 1 :
           $('#dice1').attr('src', src);
           $('#text1').html(seed+1);
-          var a1 = seed + 1 ;
+          var a = seed + 1 ;
           break;
         case 2 :
           $('#dice2').attr('src', src);
           $('#text2').html(seed+1);
-          var b1 = seed + 1 ;
+          var b = seed + 1 ;
           break;
         case 3 :
           $('#dice3').attr('src', src);
           $('#text3').html(seed+1);
-          var c1 = seed + 1;
+          var c = seed + 1;
           break;
         case 4 :
           $('#dice4').attr('src', src);
           $('#text4').html(seed+1);
-          var a2 = seed + 1 ;
+          var x = seed + 1 ;
           break;
         case 5 :
           $('#dice5').attr('src', src);
           $('#text5').html(seed+1);
-          var b2 = seed + 1 ;
+          var y = seed + 1 ;
           break;
         case 6 :
           $('#dice6').attr('src', src);
           $('#text6').html(seed+1);
-          var c2 = seed + 1 ;
+          var z = seed + 1 ;
           break;
       }
     }
       //役の条件
-      var is111 = ((a1 == 1) && (b1 == 1) && (c1 == 1));
-      var is_zorome = ((a1 != 1) && (b1 != 1) && (c1 != 1) && (a1 == b1) && (b1 == c1));
-      var is456 = ((a1 == 4) && (b1 == 5) && (c1 == 6))||((a1 == 4) && (b1 == 6) && (c1 == 5))||((a1 == 5) && (b1 == 4) && (c1 == 6))||((a1 == 5) && (b1 == 6) && (c1 == 4))||((a1 == 6) && (b1 == 4) && (c1 == 5))||((a1 == 6) && (b1 == 5) && (c1 == 4));
-      var is123 = ((a1 == 1) && (b1 == 2) && (c1 == 3))||((a1 == 1) && (b1 == 3) && (c1 == 2))||((a1 == 2) && (b1 == 1) && (c1 == 3))||((a1 == 2) && (b1 == 3) && (c1 == 1))||((a1 == 3) && (b1 == 1) && (c1 == 2))||((a1 == 3) && (b1 == 2) && (c1 == 1));
-      var is_num_a = ((b1 == c1) && (c1 != a1));
-      var is_num_b = ((c1 == a1) && (a1 != b1));
-      var is_num_c = ((a1 == b1) && (b1 != c1));
-      var yaku_against= (is111) || (is_zorome) || (is456) || (is123) || (is_num_a) || (is_num_b) || (is_num_c);
+      //
+      var is111 = a == 1 && b == 1 && c == 1;
+      var is_zorome = a != 1 && b != 1 && c != 1 && a == b && b == c;
+      var is456 = (a == 4 && b == 5 && c == 6)||(a == 4 && b == 6 && c == 5)||(a == 5 && b == 4 && c == 6)||(a == 5 && b == 6 && c == 4)||(a == 6 && b == 4 && c == 5)||(a == 6 && b == 5 && c == 4);
+      var is123 = (a == 1 && b == 2 && c == 3)||(a == 1 && b == 3 && c == 2)||(a == 2 && b == 1 && c == 3)||(a == 2 && b == 3 && c == 1)||(a == 3 && b == 1 && c == 2)||(a == 3 && b == 2 && c == 1);
+      var is_num_a = b == c && c != a;
+      var is_num_b = c == a && a != b;
+      var is_num_c = a == b && b != c;
+      var yaku_against= is111 || is_zorome || is456 || is123 || is_num_a || is_num_b || is_num_c;
       //役を表示
+      //役なしの場合がピンゾロと認定されているようだ。
       switch (yaku_against) {
         case is111 :
           var n1 = "ピンゾロ!!";
@@ -85,16 +87,16 @@ function afterLoad() {
           var n1 = "ヒフミ!!";
           break;
         case is_num_a :
-          var n1 = a;
+          var n1 = "の "+a;
           break;
         case is_num_b :
-          var n1 = b;
+          var n1 = "の "+b;
           break;
         case is_num_c :
-          var n1 = c;
+          var n1 = "の "+c;
           break;
         default :
-          var n1 = "役なし";
+          var n1 = "で役なし";
           break;
       }
     $('#score_against').html(n1);
